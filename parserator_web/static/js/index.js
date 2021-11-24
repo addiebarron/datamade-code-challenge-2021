75fd8cd312f7d760d97a77cea76efe8f8860d446
Addie <addiebarronemail@gmail.com>
2021-11-23 19:51:36 -0500

    Backend views and frontend complete

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,91 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+// Hi DataMade! I decided to use vanilla JS instead of jQuery this time,
+// since it's been more my jam lately. In practice, I would probably use
+// native HTML form handling for this use case, as it usually makes for better
+// accessibility and, in my experience, performance and code simplicity!
+
+// Wire up the main form
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("address").parentElement;
+  form.addEventListener("submit", onSubmit);
+});
+
+// Handle form submission
+const onSubmit = async (e) => {
+  // Prevent the page from reloading on submit
+  e.preventDefault();
+  // Grab the address field as a string
+  const formFields = e.target.elements;
+  const data = {
+    address: formFields.address.value,
+  };
+  // Construct the request URL
+  const url = new URL("/api/parse", window.location.toString());
+  for (let field in data) {
+    url.searchParams.append(field, data[field]);
+  }
+
+  // Send request to the API
+  try {
+    response = await fetch(url);
+    // We'll reach this on any good response, even error codes
+    handleResponse(response);
+  } catch (err) {
+    // We should only get here if the request fails (e.g. network failure)
+    handleRequestError(err);
+  }
+};
+
+// Process a response from the API
+const handleResponse = async (res) => {
+  // Convert response body to an object
+  const data = await res.json();
+  // Get references to some needed elements
+  const resultsDiv = document.getElementById("address-results");
+  const errorDiv = document.getElementById("error");
+
+  if (res.ok) {
+    // Hide errors section
+    errorDiv.classList.add("d-none");
+    // Get element references
+    const resultsTableBody = resultsDiv.querySelector("table>tbody");
+    const typeSpan = document.getElementById("parse-type");
+    // Display the address type
+    typeSpan.innerText = data.address_type;
+    // Clear out any previous table rows
+    resultsTableBody.innerHTML = "";
+    // Write rows to the table
+    for (let [part, tag] of Object.entries(data.address_components)) {
+      const tableRow = document.createElement("tr");
+      tableRow.innerHTML = `<td>${tag}</td><td>${part}</td>`;
+      resultsTableBody.appendChild(tableRow);
+    }
+    // Show results section
+    resultsDiv.classList.remove("d-none");
+  } else {
+    renderError(data.error.title, data.error.detail);
+  }
+};
+
+// Handle any errors thrown when sending the request
+const handleRequestError = (err) => {
+  console.error(err);
+  renderError(
+    "Could not contact the server",
+    "Check your network connection and try again."
+  );
+};
+
+// Print an error message to the page
+const renderError = (title, detail) => {
+  const resultsDiv = document.getElementById("address-results");
+  const errorDiv = document.getElementById("error");
+  const errorP = errorDiv.querySelector("p");
+  const errorH4 = errorDiv.querySelector("h4");
+  // Hide results section
+  resultsDiv.classList.add("d-none");
+  // Populate error message
+  errorH4.innerText = title;
+  errorP.innerText = detail;
+  // Show error section
+  errorDiv.classList.remove("d-none");
+};
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError
+from datetime import datetime
 
 
 class Home(TemplateView):
@@ -14,11 +14,61 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        '''
+        Handle GET requests to this endpoint.
+        '''
+
+        input_string = request.query_params.get('address')
+
+        # Parse the address
+        try:
+            tagged_address, address_type = self.parse(input_string)
+        # Handle RepeatedLabelError
+        except usaddress.RepeatedLabelError as e:
+            timestamp = str(datetime.now())
+            status = 500
+            response_data = {
+                'status': status,
+                'error': {
+                    'timestamp': timestamp,
+                    'input_string': input_string,
+                    'title': 'Invalid input string',
+                    'detail': 'The address could not be parsed because of a repeated label.',
+                    'path': '/api/parse'
+                }
+            }
+        # Handle all other errors
+        except:
+            timestamp = str(datetime.now())
+            status = 500
+            response_data = {
+                'status': status,
+                'error': {
+                    'timestamp': timestamp,
+                    'input_string': input_string,
+                    'title': 'Internal server error',
+                    'detail': 'The server encountered an error parsing that address.',
+                    'path': '/api/parse',
+                }
+            }
+        # If there are no errors
+        else:
+            status = 200
+            response_data = {
+                'status': status,
+                'input_string': input_string,
+                'address_components': tagged_address,
+                'address_type': address_type,
+            }
+
+        return Response(data=response_data, status=status)
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        '''
+        Use the usaddress module to parse a given address.
+        Returns a tuple containing the type (string) and components (OrderedDict) of the address.
+        '''
+
+        address_components, address_type = usaddress.tag(address)
+
         return address_components, address_type
